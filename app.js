2b31e3e42ac4315eb0e00d596e9934bd924ca742
Bayu K <50491719+bayukembara@users.noreply.github.com>
2023-05-23 23:31:35 +0700

    server

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    const method = req.method;
+    if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>Home Page</title></head>");
+        res.write(
+            "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+        );
+        res.write("</html>");
+        return res.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        fs.writeFileSync("message.txt", "Dummy");
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+    }
+    console.log(req.url, req.method, req.headers, req.pipe);
+    res.setHeader("Content-Type", "text/html");
 });
 
 server.listen(3000);
